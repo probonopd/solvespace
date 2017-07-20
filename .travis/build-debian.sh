71c5246a1e50dc34a39c3ac36330b0f81f027444
probonopd <probonopd@users.noreply.github.com>
2017-07-20 22:12:52 +0200

    Update build-debian.sh

--- .travis/build-debian.sh
+++ .travis/build-debian.sh
@@ -9,5 +9,16 @@ cmake .. -DCMAKE_C_COMPILER=clang-3.9 -DCMAKE_CXX_COMPILER=clang++-3.9 \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DENABLE_GUI=OFF \
   -DENABLE_SANITIZERS=ON
-make VERBOSE=1
+  -DCMAKE_INSTALL_PREFIX=/usr
+make VERBOSE=1 -j$(nproc)
 make test_solvespace
+
+# Generate AppImage
+make DESTDIR=appdir install ; find appdir/
+wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" 
+chmod a+x linuxdeployqt*.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage
+find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+curl --upload-file ./SolveSpace*.AppImage https://transfer.sh/SolveSpace-git.$(git rev-parse --short HEAD)-x86_64.AppImage
