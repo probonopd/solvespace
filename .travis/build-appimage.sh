b7bb9fc34cc6315be6a556aee0cefeee19d1ee81
probonopd <probonopd@users.noreply.github.com>
2019-04-21 08:17:53 +0200

    Use relative usr/share
    https://github.com/solvespace/solvespace/pull/396

--- .travis/build-appimage.sh
+++ .travis/build-appimage.sh
@@ -11,7 +11,6 @@ cmake .. \
 make -j$(nproc)
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
 rm appdir/usr/share/solvespace/freedesktop/solvespace-*.png
-( cd appdir/usr ; ln -s ./share/solvespace ./res ) # https://github.com/solvespace/solvespace/issues/347#issuecomment-485222466
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
