938f570219bf5f687e52fe02e0c647577b3476fd
probonopd <probonopd@users.noreply.github.com>
2019-04-21 08:13:52 +0200

    rm appdir/usr/share/solvespace/freedesktop/solvespace-*.png

--- .travis/build-appimage.sh
+++ .travis/build-appimage.sh
@@ -10,6 +10,7 @@ cmake .. \
   -DENABLE_SANITIZERS=ON
 make -j$(nproc)
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
+rm appdir/usr/share/solvespace/freedesktop/solvespace-*.png
 ( cd appdir/usr ; ln -s ./share/solvespace ./res ) # https://github.com/solvespace/solvespace/issues/347#issuecomment-485222466
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
