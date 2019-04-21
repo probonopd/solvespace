f0797f034a186af07d12de18daf3c5e954de971c
probonopd <probonopd@users.noreply.github.com>
2019-04-21 07:48:53 +0200

    Symlink ../res

--- .travis/build-appimage.sh
+++ .travis/build-appimage.sh
@@ -13,7 +13,7 @@ cmake .. \
   -DENABLE_SANITIZERS=ON
 make -j$(nproc)
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
-
+( cd appdir/usr ; ln -s ./share/solvespace ./res ) # https://github.com/solvespace/solvespace/issues/347#issuecomment-485222466
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
