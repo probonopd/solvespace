6f801eff6ea435bc07d6423f1d5f6ee850da01c9
probonopd <probonopd@users.noreply.github.com>
2019-05-07 19:28:05 +0200

    Keep /usr/share/solvespace/freedesktop/
    https://github.com/solvespace/solvespace/issues/347#issuecomment-490158234

--- .travis/build-appimage.sh
+++ .travis/build-appimage.sh
@@ -10,8 +10,6 @@ cmake .. \
   -DENABLE_SANITIZERS=ON
 make -j$(nproc)
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
-rm appdir/usr/share/solvespace/freedesktop/solvespace-*.png
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
