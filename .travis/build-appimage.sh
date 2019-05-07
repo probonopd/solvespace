ff4575b43682cc12ba1fa5b5c755a6f5815fbab5
probonopd <probonopd@users.noreply.github.com>
2019-05-07 18:48:21 +0000

    strip appdir/usr/bin/solvespace

--- .travis/build-appimage.sh
+++ .travis/build-appimage.sh
@@ -10,6 +10,7 @@ make -j$(nproc)
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
 cp appdir/usr/share/icons/hicolor/48x48/apps/solvespace.png appdir/
 rm -rf appdir/usr/lib/x86_64-linux-gnu appdir/usr/bin/solvespace-cli
+strip appdir/usr/bin/solvespace
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
