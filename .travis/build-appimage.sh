b1b534e2fc3d27b233405426ea42fd5a7e95f5e1
probonopd <probonopd@users.noreply.github.com>
2019-09-05 07:07:09 +0000

    -exclude-libs=libgtk-3.so.0,libgdk-3.so.0
    https://github.com/solvespace/solvespace/issues/347#issuecomment-528229767

--- .travis/build-appimage.sh
+++ .travis/build-appimage.sh
@@ -13,4 +13,4 @@ rm -rf appdir/usr/lib/x86_64-linux-gnu appdir/usr/bin/solvespace-cli
 strip appdir/usr/bin/solvespace
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -exclude-libs=libgtk-3.so.0,libgdk-3.so.0
