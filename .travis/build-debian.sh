2a46761e4899535916040a3e942869bdff91a3d2
probonopd <probonopd@users.noreply.github.com>
2020-01-16 21:54:58 +0000

    Update build-debian.sh

--- .travis/build-debian.sh
+++ .travis/build-debian.sh
@@ -6,6 +6,13 @@ mkdir build
 cd build
 cmake .. \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-  -DENABLE_SANITIZERS=ON
-make VERBOSE=1
+  -DENABLE_SANITIZERS=ON \
+  -DCMAKE_INSTALL_PREFIX=/usr
+make VERBOSE=1 -j$(nproc)
 make test_solvespace
+make DESTDIR=appdir -j$(nproc) install ; find appdir/
+
+wget -c -nv https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases -O - | grep "appimagetool-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
+chmod +x ./appimagetool-*.AppImage
+./appimagetool-*.AppImage deploy appdir/usr/share/applications/*.desktop
+./appimagetool-*.AppImage appdir/
